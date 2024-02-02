45fc0b593da36b5902b71189701b80b3348cfca4
Robert Odrowaz <robert@odrowaz.dev>
2024-02-02 22:36:52 +0100

    Initial commit

---
+++ bin/arb_gpt_translator.dart
@@ -0,0 +1,5 @@
+import 'package:arb_gpt_translator/arb_gpt_translator.dart' as arb_gpt_translator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${arb_gpt_translator.calculate()}!');
+}
---
+++ test/arb_gpt_translator_test.dart
@@ -0,0 +1,8 @@
+import 'package:arb_gpt_translator/arb_gpt_translator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
