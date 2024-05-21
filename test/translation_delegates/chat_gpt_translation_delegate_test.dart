dd2e1c98d29c81d6394d219d193f3ee183bf8e7c
Robert Odrowaz <robert@odrowaz.dev>
2024-05-21 21:07:18 +0200

    Add smoke tests for translation delegates

---
+++ test/translation_delegates/chat_gpt_translation_delegate_test.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:arb_translate/src/translate_options/translate_options.dart';
+import 'package:arb_translate/src/translation_delegates/chat_gpt_translation_delegate.dart';
+import 'package:test/test.dart';
+
+import 'util.dart';
+
+void main() {
+  group(
+    'ChatGptTranslationDelegate',
+    () {
+      setUpAll(
+        () {
+          if (Platform.environment['ARB_TRANSLATE_OPEN_AI_API_KEY']?.isEmpty ??
+              true) {
+            throw Exception(
+                'Missing ARB_TRANSLATE_OPEN_AI_API_KEY environment variable');
+          }
+        },
+      );
+
+      ChatGptTranslationDelegate createDelegate(Model model) {
+        return ChatGptTranslationDelegate(
+          model: model,
+          apiKey: Platform.environment['ARB_TRANSLATE_OPEN_AI_API_KEY']!,
+          context: context,
+          useEscaping: false,
+          relaxSyntax: false,
+        );
+      }
+
+      for (final model in Model.gptModels) {
+        test(
+          'returns a result from ${model.name}',
+          () async {
+            await tryTranslateWithDelegate(createDelegate(model));
+          },
+        );
+      }
+    },
+  );
+}
